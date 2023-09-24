0ffaf20e956790f4a129ffeb25f5731d94e3cf3b
parikshitRoy <parikshitroy00@gmail.com>
2023-09-24 19:55:06 +0530

    Deleted Unwanted Lines

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
-import Button from "@/components/common/Button/Button";
 import Head from "next/head";
-import Link from "next/link";
 import { useSession } from "next-auth/react";
 import HomeComponent from "@/components/Home";
 
